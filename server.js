eb37e8e81d912670ed4f4bcd39dfe8ac27c5a032
demiurgefcker-coder <demiurgefcker@gmail.com>
2025-09-11 15:17:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+
+app.use(express.json({ limit: '50mb' })); 
+app.use(express.text({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 app.use(express.text()); // basit text POST için
 
 // POST isteğiyle veri al
@@ -12,4 +17,5 @@ app.post("/data", (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server çalışıyor, port: ${PORT}`);
+
 });
